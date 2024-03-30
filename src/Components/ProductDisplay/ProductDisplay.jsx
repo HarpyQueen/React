0445a782c75fa4b61729b7d504259c0b3c7ab311
Binita Karki <binitakarki3355@gmail.com>
2024-03-30 13:03:44 +0545

    Added my project

--- src/Components/ProductDisplay/ProductDisplay.jsx
+++ src/Components/ProductDisplay/ProductDisplay.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
 import './ProductDisplay.css'
-import star_icon from '../Assets/star_icon.png';
-import star_dull_icon from "../Assets/star_dull_icon.png";
 const ProductDisplay = (props) => {
     const {product} = props;
   return (
@@ -19,15 +17,29 @@ const ProductDisplay = (props) => {
         </div>
         <div className="productdisplay-right">
             <h1>{product.name}</h1>
-            <div className="productdisplay-right-star">
-                <img src={star_icon} alt="" />
-                <img src={star_icon} alt="" />
-                <img src={star_icon} alt="" />
-                <img src={star_icon} alt="" />
-                <img src= {star_dull_icon} alt="" />
-                <p>(122)</p>
+            <div className="productdisplay-right-prices">
+              <div className="productdisplay-right-price-old">${product.old_price}</div>
+              <div className="productdisplay-right-price-new">${product.new_price}</div>
             </div>
+            <div className="productdisplay-right-description">
+                A lightweight, usually knitted, pullover shirt, close-fitting garment.
+            </div>
+            <div className="productdisplay-right-size">
+                <h1>Select Size</h1>
+                <div className="productdisplay-right-size">
+                    <div>S</div>
+                    <div>M</div>
+                    <div>L</div>
+                    <div>XL</div>
+                    <div>XXL</div>
+                
+                </div>            
+            </div>
+            <button>ADD TO CART</button>
+            <p className="productdisplay-right-category"> <span>Category :</span> Women, T-shirt, Crop Top</p>
+            <p className="productdisplay-right-category"> <span>Tags :</span> Modern, Latest</p>
         </div>
+
     </div>
   )
 }
